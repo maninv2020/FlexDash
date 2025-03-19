c6d65d3bb35d2c943b7f98ba6c33a1e3d6323228
Mani NV <maninv2020@gmail.com>
2025-03-19 21:47:19 +0530

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 import "../styles/main.css";
+import jQuery from "jquery";
+
+jQuery(function () {
+	let $buttons = jQuery(".accordion-btn"),
+		$contents = jQuery(".accordion-content");
+
+	$buttons.on("click", function () {
+		let $this = jQuery(this),
+			$content = $this.next(".accordion-content");
+
+		if (!$this.hasClass("active")) {
+			$buttons.removeClass("text-black active");
+			$contents.slideUp();
+			$this.addClass("text-black active");
+			$content.slideDown();
+		} else {
+			$this.removeClass("text-black active");
+			$content.slideUp();
+		}
+	});
+});
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
 	server: {
 		port: 3000, // Change port if needed
 	},
-	plugins: [tailwindcss()],
+	plugins: [
+		tailwindcss(),
+		inject({
+			$: "jquery", // Automatically import jQuery as $
+			jQuery: "jquery", // Allow usage as jQuery too
+		}),
+	],
 });
